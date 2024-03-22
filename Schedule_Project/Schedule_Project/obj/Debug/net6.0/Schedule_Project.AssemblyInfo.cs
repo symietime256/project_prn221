48b14dcdbc266340d014155dc1e4a273e78920dd
Nguyen Duc Minh <mikawabang07@gmail.com>
2024-03-22 23:23:09 +0700

    fix: fix link update

--- Schedule_Project/Schedule_Project/obj/Debug/net6.0/Schedule_Project.AssemblyInfo.cs
+++ Schedule_Project/Schedule_Project/obj/Debug/net6.0/Schedule_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Schedule_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1984b3afeec7c2ef53ba949d6d931f855416419")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+831a3e3f447c9ba5905ece56127837bd04ea35eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Schedule_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Schedule_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
